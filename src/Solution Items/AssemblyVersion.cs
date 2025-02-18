1eb1460606e4c55188ad47ccd06c576f9942d471
Hirogen <hirogenat@gmail.com>
2025-02-18 20:00:30 +0100

    assembly version update 1.11.1

--- src/Solution Items/AssemblyVersion.cs
+++ src/Solution Items/AssemblyVersion.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("LogExpert")]
 [assembly: AssemblyCopyright("Original work Copyright (c) 2008-2011 Hagen Raab\r\nModified work Copyright (c) 2025 Zarunbal|Hirogen and many others")]
 
-[assembly: AssemblyVersion("1.10.0")]
-[assembly: AssemblyFileVersion("1.10.0")]
-[assembly: AssemblyInformationalVersion("1.10.0")]
+[assembly: AssemblyVersion("1.11.1")]
+[assembly: AssemblyFileVersion("1.11.1")]
+[assembly: AssemblyInformationalVersion("1.11.1")]
 
 [assembly: ComVisible(false)]
